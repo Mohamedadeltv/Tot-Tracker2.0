238159886c6aa7fc29e2fb31e1b99480c7e33e0d
fadyamr909 <fadyamr1999@gmail.com>
2023-05-07 16:45:51 +0300

    monitor

--- lib/NEW_SCREENS/app_drawer.dart
+++ lib/NEW_SCREENS/app_drawer.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:tottracker/NEW_SCREENS/features_overview_screen.dart';
 import 'package:provider/provider.dart';
+=======
+import 'package:tottracker/NEW_SCREENS/drag.dart';
+import 'package:tottracker/NEW_SCREENS/feedback_screen.dart';
+import 'package:tottracker/NEW_SCREENS/sleep_monitor.dart';
+import 'package:tottracker/NEW_SCREENS/help_screen.dart';
+import 'package:tottracker/NEW_SCREENS/invite_friend_screen.dart';
+>>>>>>> Stashed changes
 import 'package:tottracker/NEW_SCREENS/profile_screen.dart';
 import 'package:tottracker/api/auth_repositry.dart';
 
@@ -25,31 +33,75 @@ class AppDrawer extends StatelessWidget {
               leading: Icon(Icons.person),
               title: Text('Profile'),
               onTap: () {
+<<<<<<< Updated upstream
                 Navigator.of(context)
                     .pushReplacementNamed(ProfileScreen.routeName);
+=======
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfileScreens(),
+                    ));
+>>>>>>> Stashed changes
               }),
           ListTile(
               leading: Icon(Icons.shop),
               title: Text('Features'),
               onTap: () {
+<<<<<<< Updated upstream
                 Navigator.of(context)
                     .pushReplacementNamed(FeaturesOverviewScreen.routeName);
+=======
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DashScreen(),
+                    ));
+>>>>>>> Stashed changes
               }),
           ListTile(
               leading: Icon(Icons.mail),
               title: Text('Contact'),
               onTap: () {
+<<<<<<< Updated upstream
                 Navigator.of(context)
                     .pushReplacementNamed(FeaturesOverviewScreen.routeName);
+=======
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => FeedbackScreen(),
+                    ));
+>>>>>>> Stashed changes
               }),
           ListTile(
               leading: Icon(Icons.question_mark),
               title: Text('FAQ'),
               onTap: () {
+<<<<<<< Updated upstream
                 Navigator.of(context)
                     .pushReplacementNamed(FeaturesOverviewScreen.routeName);
               }),
           ListTile(
+=======
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => InviteFriend(),
+                    ));
+              }),
+          ListTile(
+              leading: Icon(Icons.group),
+              title: Text('Manage Bracelets'),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ArrowWidget(),
+                    ));
+              }),
+          ListTile(
+>>>>>>> Stashed changes
               leading: Icon(Icons.exit_to_app),
               title: Text('Logout'),
               onTap: () {
