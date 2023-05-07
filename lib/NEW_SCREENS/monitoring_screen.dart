31cf620519bdda7d537cc41e98febe04aac31b46
fadyamr909 <fadyamr1999@gmail.com>
2023-05-07 16:59:52 +0300

    monitor

---
+++ lib/NEW_SCREENS/monitoring_screen.dart
@@ -0,0 +1,200 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tottracker/NEW_SCREENS/features_overview_screen.dart';
+import 'package:tottracker/providers/profile_controller.dart';
+import '../providers/user.dart' as U;
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+import 'package:flutter/material.dart';
+
+class BoxDash extends StatelessWidget {
+  final String Text1;
+  final String Text2;
+  final String Text3;
+  final IconData icon;
+  final Color color;
+
+  BoxDash(
+      {required this.Text1,
+      required this.Text2,
+      required this.Text3,
+      required this.color,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      elevation: 8,
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  Text1,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  Text2,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+                Text(
+                  Text3,
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900),
+                ),
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: color,
+              radius: 35,
+              child: Icon(
+                icon,
+                color: Colors.white,
+                size: 35,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RowCard extends StatelessWidget {
+  final String Text1;
+  final String Text2;
+  final IconData icon;
+  final Color color;
+
+  RowCard(
+      {required this.Text1,
+      required this.Text2,
+      required this.color,
+      required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        elevation: 8,
+        margin: EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: color,
+                radius: 35,
+                child: Icon(
+                  icon,
+                  color: Colors.white,
+                  size: 35,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    Text1,
+                    style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  Text(
+                    Text2,
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w900),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DashScreen extends StatelessWidget {
+  const DashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text(
+          "Baby monitor",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BoxDash(
+              Text1: "Status",
+              Text2: "Sleeping",
+              Text3: "",
+              color: Colors.blueAccent,
+              icon: Icons.offline_pin,
+            ),
+            Row(
+              children: [
+                RowCard(
+                  Text1: "Blood Pressure",
+                  Text2: "50",
+                  color: Colors.orange,
+                  icon: Icons.send,
+                ),
+                RowCard(
+                  Text1: "Temperature",
+                  Text2: "37",
+                  color: Colors.grey,
+                  icon: Icons.group,
+                ),
+              ],
+            ),
+            BoxDash(
+              Text1: "Growth monitoring",
+              Text2: "weght:",
+              Text3: "height:",
+              color: Colors.green,
+              icon: Icons.email,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
