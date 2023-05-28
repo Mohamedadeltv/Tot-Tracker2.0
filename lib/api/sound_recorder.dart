8efebfb5c55574d6c89455b5946fba0d3d8adf98
mohamed adel <mohamddadell@gmail.com>
2023-05-28 20:33:00 +0300

    .

--- lib/api/sound_recorder.dart
+++ lib/api/sound_recorder.dart
@@ -4,6 +4,8 @@ import 'package:flutter_sound_lite/public/flutter_sound_recorder.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SoundRecorder {
   FlutterSoundRecorder? _audioRecorder;
@@ -31,35 +33,61 @@ class SoundRecorder {
     _isRecordInitialized = false;
   }
 
-  Future<void> _record() async {
-    if (!_isRecordInitialized) return;
-    final appDocumentsDirectory = await getApplicationDocumentsDirectory();
-    final String folderName = 'my_audio_folder';
-    final String fileName = 'audio-$audioNumber.aac';
 
-    Directory currentDirectory = Directory.current;
-    String projectDirectoryPath = currentDirectory.path;
+Future<void> _record() async {
+  if (!_isRecordInitialized) return;
 
-    final String folderPath = '$projectDirectoryPath/$folderName';
-    final String filePath = '$folderPath/$fileName';
+  final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+  final String folderName = 'my_audio_folder';
+  final String fileName = 'audio-$audioNumber.wav';
 
-    Directory folderDirectory = Directory(folderPath);
-    folderDirectory.createSync(recursive: true);
+  final folderPath = '${appDocumentsDirectory.path}/$folderName';
+  final filePath = '$folderPath/$fileName';
 
-    final pathToSaveAudio = '${folderDirectory.path}/$fileName';
+  Directory(folderPath).createSync(recursive: true);
 
-    await _audioRecorder!.startRecorder(
-      toFile: pathToSaveAudio,
-      codec: Codec.aacADTS,
-    );
+  final pathToSaveAudio = '$folderPath/$fileName';
 
-    audioNumber++;
+  await _audioRecorder!.startRecorder(
+    toFile: pathToSaveAudio,
+    codec: Codec.pcm16WAV, // Use WAV codec for audio recording
+  );
+
+  print(filePath);
+  audioNumber++;
+}
+
+Future<void> _stop() async {
+  if (!_isRecordInitialized) return;
+  
+  await _audioRecorder!.stopRecorder();
+
+  final appDocumentsDirectory = await getApplicationDocumentsDirectory();
+  final String folderName = 'my_audio_folder';
+  final String fileName = 'audio-$audioNumber.wav';
+
+  final folderPath = '${appDocumentsDirectory.path}/$folderName';
+  final filePath = '$folderPath/$fileName';
+
+  final pathToSaveAudio = '$folderPath/$fileName';
+
+  // Read the audio file as bytes
+  final audioBytes = File(pathToSaveAudio).readAsBytesSync();
+
+  // Convert audio bytes to Base64 string
+  final audioBase64 = base64Encode(audioBytes);
+
+  // Send audioBase64 to Flask server
+  final ngrokUrl = 'https://0131-156-192-20-83.ngrok-free.app';
+  final apiUrl = '$ngrokUrl/api/predict';
+
+  final response = await http.post(Uri.parse(apiUrl), body: {'audio': audioBase64});
+  if (response.statusCode == 200) {
+    print(response);
+  } else {
+    print('Error sending audio to Flask server');
   }
-
-  Future<void> _stop() async {
-    if (!_isRecordInitialized) return;
-    await _audioRecorder!.stopRecorder();
-  }
+}
 
   Future<void> toggleRecording() async {
     if (_audioRecorder!.isStopped) {
